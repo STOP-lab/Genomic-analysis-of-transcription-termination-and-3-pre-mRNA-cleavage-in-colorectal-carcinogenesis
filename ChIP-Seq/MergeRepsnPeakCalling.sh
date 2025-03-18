89362af9776766f9e27654aa0efe458673980778
STOP-lab <gdulalab@gmail.com>
2025-03-18 15:43:30 +0100

    Update MergeRepsnPeakCalling.sh

--- ChIP-Seq/MergeRepsnPeakCalling.sh
+++ ChIP-Seq/MergeRepsnPeakCalling.sh
@@ -11,8 +11,10 @@ samtools merge -hb Replicates/Merged-BAMs/SW480_input_Reps-Merged.bam Aligned/Sp
 samtools merge -hb Replicates/Merged-BAMs/SW620_PCF11_Reps-Merged.bam Aligned/Split_BAM/SW620_PCF11_rep1-Human.bam Aligned/Split_BAM/SW620_PCF11_rep2-Human.bam Aligned/Split_BAM/SW620_PCF11_rep3-Human.bam --threads 12
 samtools merge -hb Replicates/Merged-BAMs/SW620_input_Reps-Merged.bam Aligned/Split_BAM/SW620_input_rep1-Human.bam Aligned/Split_BAM/SW620_input_rep2-Human.bam Aligned/Split_BAM/SW620_input_rep3-Human.bam --threads 12
 
-# Index the merged BAMs
+## Index the merged BAMs and convert them to bigWigs
 for f in Replicates/Merged-BAMs/*.bam ; do samtools index  -@ 12 -b "$f"; done;
+
+for f in Replicates/Merged-BAMs/*.bam ; do bamCoverage -b "$f" --binSize 50 --normalizeUsing CPM -p 18 -o "$f".bw
 
 ## Peak Calling with MACS2
 macs2 callpeak -t Replicates/Merged-BAMs/1CT_PCF11_Reps-Merged.bam -c Replicates/Merged-BAMs/HCEC_1CT_input_Reps-Merged.bam -f BAMPE -g hs --outdir  Replicates/Peaks/Merged-BAMs/ -n 1CT_PCF11_Reps-MergedBAM --nomodel --broad -q 0.01 --broad-cutoff 0.01 2> Replicates/Peaks/Merged-BAMs/1CT_PCF11_Reps-MergedBAM_macs2.log
