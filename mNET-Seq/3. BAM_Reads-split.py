e9dfecb0882ecee8c96c0cfab2e9745b9eec6fcf
STOP-lab <gdulalab@gmail.com>
2025-03-18 16:29:52 +0100

    Update 3. BAM_Reads-split.py

--- mNET-Seq/3. BAM_Reads-split.py
+++ mNET-Seq/3. BAM_Reads-split.py
@@ -5,16 +5,20 @@ for srr in open("Samples_mNET.txt"):
 	
 ## Strand-specific BAMs
 ########### Forward Strand reads - 5' to 3' direction ###########
-	os.system("samtools view -bh -@ 18 -f 64 -F 16 Replicates/Merged-BAMs"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd1.bam")
-	os.system("samtools view -bh -@ 18 -f 144 Replicates/Merged-BAMs"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd2.bam")
+##Extracting the alignments of first in pair if they map to the forward strand
+	os.system("samtools view -bh -@ 18 -f 64 -F 16 Replicates/Merged-BAMs/"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd1.bam")
+##Extracting the alignments of the second in pair if they map to the reverse strand
+	os.system("samtools view -bh -@ 18 -f 144 Replicates/Merged-BAMs/"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd2.bam")
 
 ## Merge both the fwd1 and fwd2 bams
 	os.system("samtools merge -f -@ 18 Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd.bam Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd1.bam Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd2.bam")
 	os.system("samtools index -@ 18 -b Replicates/Stranded_BAMs/"+sra+"-Reps_merged_fwd.bam")
  
 ########### Reverse Strand reads - 3' to 5' direction ###########
-	os.system("samtools view -bh -@ 18 -f 80 Replicates/Merged-BAMs"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev1.bam")
-	os.system("samtools view -bh -@ 18 -f 128 -F 16 Replicates/Merged-BAMs"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev2.bam")
+##Extracting the alignments of first in pair if they map to the reverse strand
+	os.system("samtools view -bh -@ 18 -f 80 Replicates/Merged-BAMs/"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev1.bam")
+##Extracting the alignments of the second in pair if they map to the forward strand
+	os.system("samtools view -bh -@ 18 -f 128 -F 16 Replicates/Merged-BAMs/"+sra+"-Reps_merged.bam > Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev2.bam")
 
 ## Merge rev1 and rev2 bams
 	os.system("samtools merge -f -@ 18 Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev.bam Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev1.bam Replicates/Stranded_BAMs/"+sra+"-Reps_merged_rev2.bam")
