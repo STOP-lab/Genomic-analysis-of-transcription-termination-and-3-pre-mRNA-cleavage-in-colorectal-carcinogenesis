7f21685537e001bd64a24b6ec3b7ca9dedb8434d
STOP-lab <gdulalab@gmail.com>
2025-03-24 16:18:25 +0100

    Update 1.3Prime-Seq_processing.py

--- 3'mRNA-Seq/1.3Prime-Seq_processing.py
+++ 3'mRNA-Seq/1.3Prime-Seq_processing.py
@@ -9,8 +9,6 @@ for srr in open("accession_id.txt"):
 ## Trimming Poly-A/T tail using -a A{20}/T{20} option
 	os.system("trim_galore -j 4 3Prime-Seq/Trimmed/"+sra+"_trimmed.fq.gz -a A{20} -a2 T{20} --basename "+sra+"-PolyA -o 3Prime-Seq/Trimmed/PolyA-T/ --fastqc_args '--threads 12 --outdir 3Prime-Seq/QC/Trimmed_FastQC/PolyA-T'")
 	
-	os.system("trim_galore -j 4 3Prime-Seq/Trimmed/PolyA/"+sra+"-PolyA_trimmed_trimmed.fq.gz -a T{20} --basename "+sra+"-PolyA-T -o 3Prime-Seq/Trimmed/PolyA-T/ --fastqc_args '--threads 12 --outdir 3Prime-Seq/QC/Trimmed_FastQC/PolyA-T'")
-
 ### STAR Alignment 
 	os.system("STAR --genomeDir /home/micgdu/GenomicData/genomicIndices/hsapiens/STAR/STAR-2.7.6a_GRCh38_120bp/ --runThreadN 15 --readFilesCommand zcat --readFilesIn Trimmed/PolyA-T/"+sra+"-PolyA-T_trimmed_trimmed_trimmed.fq.gz --outFileNamePrefix 3Prime-Seq/Aligned/"+sra+"-STAR --outFilterType BySJout --outFilterMultimapNmax 200 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.6 --alignIntronMin 20 --alignIntronMax 1000000 --outSAMtype BAM SortedByCoordinate --outSAMattributes NH HI NM MD --quantMode GeneCounts --genomeLoad LoadAndKeep")
 
