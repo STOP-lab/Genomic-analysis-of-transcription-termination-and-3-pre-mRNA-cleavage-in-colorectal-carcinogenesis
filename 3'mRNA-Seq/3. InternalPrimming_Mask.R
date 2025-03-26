3c2420dc7eac7fd9eeebdca7beeed7f0c2849a17
STOP-lab <gdulalab@gmail.com>
2025-03-26 10:43:18 +0100

    Update and rename 3. InternalPrimming_Mask.sh to 3. InternalPrimming_Mask.R

--- 3'mRNA-Seq/3. InternalPrimming_Mask.R
+++ 3'mRNA-Seq/3. InternalPrimming_Mask.R
@@ -1,23 +1,16 @@
-#### filtering of Lexogen QuantSeq Rev data to keep only genuine polyadenylation sites & remove signal internal primed reads 
-
-### Filtering out of internal priming events; modified from Derti et al. Genome Res. 2012. 22: 1173-1183 (consider upadting of APAs from https://academic.oup.com/nar/article/48/D1/D174/5588346)  
+#Genomic Mask generation
 
 # Overview:
-# - find reads, where the genomic sequence directly downstream contains a stretch of As: potential internal priming (Ts on the - strand)               
-#     -- > 6/10 bases are A
-#     --6 consecutive bases in the 10nt window are A
-# 
-# 2. exclude from the mask:
-# - same potential sites that overlap +-20bp a 3'end from GENCODE annptaion (gtf file): 
-# - exclude polyA sites from Derti et al. 2012 (downloaded from UCSC table browser, available also form GEO)
-# [optional 2: looking for overlap with known 3'ends might be good enough, otherwise exclude also potential internal priming sites containing PAS signal (see below) -40 to -10 from 3' end]
+# 1.Includes regions where the genomic sequence contain a stretch of A’s (forward strand) or T’s (reverse strand) within the following conditions              
+#	- Six or more consecutive A/T bases in a 10 nucleotide window 	
+#	- More than 6 A/T bases in a 10 nucletide window
+#
+# 2.Exclude from the mask:
+# - Potential sites that overlap +-20bp a 3'end from GENCODE annotation (gtf file): 
+# - Exclude polyA sites from Derti et al. 2012 (downloaded from UCSC table browser, available also form GEO)
 
-### 1. creating new GTF files with annotation for beginnings and end of the whole genes
-### 2. merging all APA events form different tissues from Derti et al. 2012; data in hg19 neds to be lifted to hg38
 
-### merging all APA events form different tissues from Derti et al. 2012; data in hg19 neds to be lifted to hg38
-
-R
+### Merge all APA events form different tissues from Derti et al. 2012; data is in hg19 and needs to be lifted to hg38
 
 library(GenomicRanges)
 library(Rsamtools)
@@ -25,7 +18,7 @@ library(GenomicAlignments)
 library("rtracklayer")
 library(BSgenome.Hsapiens.UCSC.hg19)
 
-setwd("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/polyAsites/")
+setwd("3Prime-Seq/IPF/PolyAsites/")
 
 files<- list.files()
 
@@ -39,7 +32,7 @@ for (i in 1:9) {
 	p<-read.table(Fwd[i], header=FALSE, skip=1, stringsAsFactors=FALSE)
 	pG<-GRanges(p$V1,IRanges(p$V2,p$V3))
 	pG<-keepStandardChromosomes(pG, species="Homo sapiens", pruning.mode="coarse")
-	pG<-resize(pG, 20, fix="center", use.names=TRUE, ignore.strand=TRUE)  ### change of window size form 40 to 20
+	pG<-resize(pG, 20, fix="center", use.names=TRUE, ignore.strand=TRUE)
 	pG<-sortSeqlevels(pG)
 	pG<-sort(pG)
 	Flist[i]<-pG
@@ -53,7 +46,7 @@ for (i in 1:9) {
 	p<-read.table(Rev[i], header=FALSE, skip=1, stringsAsFactors=FALSE)
 	pG<-GRanges(p$V1,IRanges(p$V2,p$V3))
 	pG<-keepStandardChromosomes(pG, species="Homo sapiens", pruning.mode="coarse")
-	pG<-resize(pG, 20, fix="center", use.names=TRUE, ignore.strand=TRUE) ### change of window size form 40 to 20
+	pG<-resize(pG, 20, fix="center", use.names=TRUE, ignore.strand=TRUE)
 	pG<-sortSeqlevels(pG)
 	pG<-sort(pG)
 	Rlist[i]<-pG
@@ -88,12 +81,11 @@ RwdTSEr_hg38<-LiftGR(RwdTSEr, hg19ToHg38)
 write.table(FwdTSEr_hg38, file="polyadenylationSites_merged_hg38_Derti_GenRes_2012_FwdTSEr_7jan24.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names = TRUE)
 write.table(RwdTSEr_hg38, file="polyadenylationSites_merged_hg38_Derti_GenRes_2012_RwdTSEr_7jan24.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names = TRUE)
 # bed files uploaded to the UCSC with the header:
-# track name="T_mask4c" description="T_mask all TxA in a row & 10nt >6T which rae not 3'end or exp APAs " color=100,0,50 
 
 save(FwdTSEr_hg38, file="FwdTSEr_hg38.RData")
 save(RwdTSEr_hg38, file="RwdTSEr_hg38.RData")
 
-### genome patternes and Gencode 3' ends
+### Identify genomic patternes and Gencode 3' ends; load the libraries separately due to the genome version(hg38)
 
 library(GenomicRanges)
 library(Rsamtools)
@@ -101,11 +93,11 @@ library(GenomicAlignments)
 library(rtracklayer)
 library(BSgenome.Hsapiens.UCSC.hg38) 
 
-setwd("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter")
-genes<-read.table("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/GTF_whole_genes_hg38_geneCodeV42_7jan24.gtf",header=FALSE,stringsAsFactors=FALSE)
+setwd("3Prime-Seq/IPF/")
+genes<-read.table("/dysk2/groupFolders/deepshika/GenomicData/Basic_Isoform/gencode.v43.basic-Isoform_GeneIds.bed",header=FALSE,stringsAsFactors=FALSE)
 
-# 1) generating 20bp widows around 3' ends
-genesG<-GRanges(genes$V1,IRanges(genes$V4,genes$V5), strand=genes$V7)
+# 1) Generate 20bp widows around 3'end of each gene
+genesG<-GRanges(genes$V1,IRanges(genes$V2,genes$V3), strand=genes$V6)
 
 genesEndPlus<-genesG[which(strand(genesG)=="+")]
 start(genesEndPlus)<-end(genesEndPlus)-10
@@ -121,7 +113,7 @@ genesEndMinusDf<-as.data.frame(genesEndMinus)
 write.table(genesEndPlusDf[,1:3] ,file="genesEndPlus10.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
 write.table(genesEndMinusDf[,1:3] ,file="genesEndMinus10.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
 
-# 2) find "AAAAAA" stretch
+# 2) find "AAAAAA"/""TTTTTT" stretch
 
 hs<-getSeq(Hsapiens)
 hs<-hs[1:25]
@@ -142,7 +134,7 @@ T6matchR<-unlist(T6match, recursive=TRUE, use.names=TRUE)
 T6matchG<-GRanges(names(T6matchR),T6matchR)
 T6matchGr<-reduce(T6matchG)
 
-# 3) find windows with >6 A/T in 10 bp windows
+# 3) find windows with >6 A/T in a 10 nucleotide window range
 
 val<-c("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20", "21","22","X","Y","M")
 val<-paste("chr",val,sep="")
@@ -173,8 +165,8 @@ lt[i]<-Aper10wIndLimGr
 T10<-do.call("c", lt)
 T10<-renameSeqlevels(T10, val)
 
-# making final mask: Fwd: A6 + A10 - genesEndPlus
-# 			 Rev  T6 + T10 - genesEndMinus
+# Final mask - Fwd: A6 + A10 - genesEndPlus
+# 	       Rev:  T6 + T10 - genesEndMinus
 
 A_both<-reduce(c(A6matchGr,A10))
 T_both<-reduce(c(T6matchGr,T10))
@@ -194,8 +186,8 @@ write.table(T_10Df[,1:3] ,file="T_10.bed",quote=FALSE,sep="\t",row.names=FALSE, 
 write.table(A_maskDf[,1:3] ,file="A_mask.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
 write.table(T_maskDf[,1:3] ,file="T_mask.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
 
-load("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/polyAsites/FwdTSEr_hg38.RData")
-load("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/polyAsites/RwdTSEr_hg38.RData")
+load("3Prime-Seq/IPF/PolyAsites/FwdTSEr_hg38.RData")
+load("3Prime-Seq/IPF/PolyAsites/RwdTSEr_hg38.RData")
 
 FwdTSEr<-FwdTSEr_hg38
 RwdTSEr<-RwdTSEr_hg38
@@ -217,9 +209,3 @@ save(T_mask2,file="T_mask4.RData")
 
 write.table(A_maskDf2[,1:3] ,file="A_mask_polyAseq_hg38.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
 write.table(T_maskDf2[,1:3] ,file="T_mask_polyAseq_hg38.bed",quote=FALSE,sep="\t",row.names=FALSE, col.names=FALSE)
-
-#load("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/A_mask4.RData")
-#load("/home/micgdu/Analysis/Kinga/3endQuantSec/IntPrFilter/T_mask4.RData")
-
-
-
