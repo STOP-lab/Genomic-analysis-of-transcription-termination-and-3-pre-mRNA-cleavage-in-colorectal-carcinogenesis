20d292bf933f7c290a609eac7dca3bbe68a15cf9
STOP-lab <gdulalab@gmail.com>
2026-07-13 17:09:47 +0200

    Updated 6.SNR-Normalised_bedGraphs_Pancreatic.R

--- mNET-Seq/6. SNR-Normalised_bedGraphs_Pancreatic..R
+++ mNET-Seq/6. SNR-Normalised_bedGraphs_Pancreatic..R
@@ -1,16 +1,16 @@
 library(DESeq2)
 
-setwd("/dysk2/groupFolders/deepshika/mNET-Seq/Pancreatic_Cells/Replicates/Merged_ReadCounts/")
+setwd("Replicates/Merged_ReadCounts/")
 gdzie<-getwd()
 
 sampleFiles <- grep("STAR-SEReadsPerGene.out.tab",list.files(gdzie),value=TRUE)
 sampleFiles
 
-sampleNames<-c("BxPC3-T4ph-SNR", "MiaPaCa2-T4ph-SNR", "Panc1-T4ph-SNR")
+sampleNames<-c("MiaPaCa2-T4ph-SNR", "Panc1-T4ph-SNR")
 
-sampleCondition<-c("WT", "Mutated", "Mutated")
-replicate<-c(1,1,1)
-order<-c(1,2,3)
+sampleCondition<-c("LowSensitivity", "HighSensitivity")
+replicate<-c(1,1)
+order<-c(1,2)
 sampleTable <- data.frame(sampleName = sampleNames, fileName = sampleFiles, condition = factor(sampleCondition),replicate=factor(replicate), name=sampleNames)
 sampleTable
 
@@ -27,20 +27,18 @@ readCountsSumNorm<-readCountsSum/SF
 readCountsSumNorm
 
 ### Single Nucleotide Resolution files processing
-#setwd("/dysk2/groupFolders/deepshika/mNET-Seq/Pancreatic_Cells/Replicates/Merged_BAMs/SNR/Stranded_bedGraphs/")
-setwd("/dysk2/groupFolders/deepshika/mNET-Seq/Pancreatic_Cells/Replicates/Merged_BAMs/SNR/Stranded_bedGraphs/Markdup/")
+setwd("Replicates/SNR/Stranded_bedGraphs/")
 
 gdzie<-getwd()
 sampleFiles2 <- grep(".bedgraph",list.files(gdzie),value=TRUE)
 sampleFiles2
 
-#sampleNames2<-c("BxPC3-T4ph-dd-SNR-fwd", "BxPC3-T4ph-dd-SNR-rev", "MiaPaCa2-T4ph-dd-SNR-fwd", "MiaPaCa2-T4ph-dd-SNR-rev", "Panc1-T4ph-dd-SNR-fwd", "Panc1-T4ph-dd-SNR-rev")
-sampleNames2<-c("BxPC3-T4ph-SNR-fwd", "BxPC3-T4ph-SNR-rev", "MiaPaCa2-T4ph-SNR-fwd", "MiaPaCa2-T4ph-SNR-rev", "Panc1-T4ph-SNR-fwd", "Panc1-T4ph-SNR-rev")
+sampleNames2<-c("MiaPaCa2-T4ph-SNR-fwd", "MiaPaCa2-T4ph-SNR-rev", "Panc1-T4ph-SNR-fwd", "Panc1-T4ph-SNR-rev")
 
-sampleCondition2<-c("WT", "WT", "Mutated", "Mutated", "Mutated", "Mutated")
-replicate2<-c(1,1,1,1,1,1)
-order2 <-c(1,2,3,4,5,6)
-strand <-c("Fwd","Rev","Fwd","Rev","Fwd","Rev")
+sampleCondition2<-c("LowSensitivity", "LowSensitivity", "HighSensitivity", "HighSensitivity")
+replicate2<-c(1,1,1,1)
+order2 <-c(1,2,3,4)
+strand <-c("Fwd","Rev","Fwd","Rev")
 
 SampleFactor<-rep(readCountsSumNorm, each = 2)
 SampleFactor
@@ -51,8 +49,7 @@ sampleTable2
 chr<-c(paste("chr",1:22,sep=""),"chrX", "chrY", "chrM")
 
 bedtoolsBdgNorm<-function(x){
-#  Path <- "/dysk2/groupFolders/deepshika/mNET-Seq/Pancreatic_Cells/Replicates/Merged_BAMs/SNR/Normalised_bedGraphs/"
-  Path <- "/dysk2/groupFolders/deepshika/mNET-Seq/Pancreatic_Cells/Replicates/Merged_BAMs/SNR/Normalised_bedGraphs/Markdup/"
+  Path <- "Replicates/SNR/Normalised_bedGraphs/"
   f <- sampleTable2$SampleFactor[x]
   a <- read.table(sampleTable2[x, 1], header = FALSE, stringsAsFactors = FALSE)
   a[, 2] <- as.integer(a[, 2]) 
@@ -66,4 +63,4 @@ bedtoolsBdgNorm<-function(x){
   write.table(a, file = file.path(Path, paste(sampleTable2$name[x], "_norm.bedgraph", sep = "")), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
 }
 
-for (i in 1:6) bedtoolsBdgNorm(i)
+for (i in 1:4) bedtoolsBdgNorm(i)
