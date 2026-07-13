d6d4a6450db300a6ef3e0dc491e858faa40509cf
STOP-lab <gdulalab@gmail.com>
2026-07-13 16:59:29 +0200

    Updated 7.Termination_Windows.sh
    Updated paths and output naming conventions for bedgraph processing.

--- mNET-Seq/7.Termination_Windows.sh
+++ mNET-Seq/7.Termination_Windows.sh
@@ -1,16 +1,8 @@
-## Multiplying the reverse strand bedgraphs with -1 using macs2 bdgopt function
-for f in Replicates/Stranded_bedGraphs/Normalised_bedGraps/*rev_norm.bedgraph ; do macs2 bdgopt -i "$f" -m multiply -p -1 -o "$f"-m1; done;
-
-## Renaming reverse_norm_bedgraph files
-for f in Replicates/Stranded_bedGraphs/Normalised_bedGraps/*-m1; do mv "$f" "$(echo "$f" | sed s/rev_norm.bedgraph-m1/rev_norm-m1.bedgraph/)"; done;
+## Multiplying the reverse strand bedgraphs with -1 using the macs2 bdgopt function
+for f in Replicates/Stranded_bedGraphs/Normalised_bedGraps/*rev_norm.bedgraph; do macs2 bdgopt -i "$f" -m multiply -p -1 -o "Replicates/Normalised_bedGraphs/$(basename "$f" _norm.bedgraph)_norm_m1.bedgraph"; done
 
 ### Termination windows identification
 ## Forward strand
-for f in Replicates/Stranded_bedGraphs/Normalised_bedGraps/*fwd_norm.bedgraph ; do macs2 bdgbroadcall -i "$f" -o "$f"-bdg_peaks --no-trackline ; done;
+for f in Replicates/Normalised_bedGraps/*fwd_norm.bedgraph ; do macs2 bdgbroadcall -i "$f" -o "Termination_Windows/$(basename "$f" _norm-m1.bedgraph)-bdg_peaks-TW" --no-trackline ; done;
 ## Reverse strand
-for f in Replicates/Stranded_bedGraphs/Normalised_bedGraps/*rev_norm-m1.bedgraph ; do macs2 bdgbroadcall -i "$f" -o "$f"-bdg_peaks --no-trackline ; done;
-
-## Move files to the Termiantion_Windows folder and rename accordingly
-mv Replicates/Stranded_bedGraphs/Normalised_bedGraps/*-bdg_peaks Replicates/Termination_Windows/
-for f in Replicates/Termination_Windows/*_peaks.bed; do mv "$f" "$(echo "$f" | sed s/_norm.bedgraph//)"; done;
-for f in Replicates/Termination_Windows/*_peaks.bed; do mv "$f" "$(echo "$f" | sed s/_norm-m1.bedgraph//)"; done;
+for f in Replicates/Normalised_bedGraps/*rev_norm-m1.bedgraph ; do macs2 bdgbroadcall -i "$f" -o "$f"-bdg_peaks --no-trackline ; done;
